24bb46815e6cb83a4db2a5d8b33a89c8faa53476
nsukhwani <nikhisukhwani.ns@gmail.com>
2025-12-13 23:25:12 +0530

    introduce cmake

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -e
+
+# Set build directory
+BUILD_DIR=build
+
+# Create build directory if it doesn't exist
+mkdir -p "$BUILD_DIR"
+
+# Configure CMake
+cmake -S . -B "$BUILD_DIR" -G "MinGW Makefiles"
+
+# Build project
+cmake --build "$BUILD_DIR"
+
+# Run the executable
+"$BUILD_DIR/study.exe"
