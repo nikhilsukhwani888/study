8653cc80b39cd39b7d7787db058a251f9da08e97
nikhilsukhwani888 <nikhilsukhwani.ns@gmail.com>
2026-03-08 18:02:27 +0530

    added thread safe for singleton class with lock guard

--- designPatterns/singletonDesign/CSingleton.cpp
+++ designPatterns/singletonDesign/CSingleton.cpp
@@ -1,14 +1,17 @@
 #include "CSingleton.hpp"
 
 CSingleton* CSingleton::instance = nullptr; //The static member must be defined outside the class declaration.
+std::mutex CSingleton::mtx; // Initialize the mutex
 
 CSingleton::CSingleton()
 {
     // Constructor can be empty or initialize other members
+    std::cout << "Singleton instance created!" << std::endl;
 }
 
 CSingleton* CSingleton::getInstance()
 {
+    std::lock_guard<std::mutex> lock(mtx); // Lock the mutex to ensure thread safety
     if(instance == nullptr)
     {
         instance = new CSingleton();
--- designPatterns/singletonDesign/CSingleton.hpp
+++ designPatterns/singletonDesign/CSingleton.hpp
@@ -1,10 +1,15 @@
 #ifndef CSINGLETON_HPP
 #define CSINGLETON_HPP
 
+#include <iostream>
+#include <mutex>
+
 class CSingleton
 {
 private:
     CSingleton();
+
+    static std::mutex mtx; // Mutex for thread safety (if needed)
     static CSingleton* instance;
 
 public:
